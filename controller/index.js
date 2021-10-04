4e6a3dacddc82f262e567f0558f4c987d5013bda
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-10-04 05:04:14 -0400

    handlebars added for the tech blog

--- controller/index.js
+++ controller/index.js
@@ -4,5 +4,14 @@ const router = require('express').Router();
 router.get('/', async (req, res) => {
     res.render('homepage');
 });
+router.get('/dashboard', async (req, res) => {
+    res.render('dashboard');
+});
+router.get('/login', async (req, res) => {
+    res.render('login');
+});
+router.get('/signup', async (req, res) => {
+    res.render('signup');
+});
 
 module.exports = router;
