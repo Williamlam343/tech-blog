5003f8a44d4fb04509685adaf4a59f2f97ef0b57
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-10-06 00:42:35 -0400

    heroku fix?

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,15 @@
+if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        logging: true //false
+    });
+} else {
+    // the application is executed on the local machine
+    sequelize = new Sequelize("postgres:///my_db");
+}
+
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
