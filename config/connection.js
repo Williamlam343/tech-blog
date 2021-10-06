faa875a053a9967abc51fbcaa4ef6b89a0b057c2
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-10-06 01:29:59 -0400

    try again

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-
+let sequelize;
 if (process.env.DATABASE_URL) {
     // the application is executed on Heroku ... use the postgres database
     sequelize = new Sequelize(process.env.DATABASE_URL, {
@@ -13,20 +13,20 @@ if (process.env.DATABASE_URL) {
     sequelize = new Sequelize("postgres:///my_db");
 }
 
-let sequelize;
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        }
-    );
-}
+
+// if (process.env.JAWSDB_URL) {
+//     sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//     sequelize = new Sequelize(
+//         process.env.DB_NAME,
+//         process.env.DB_USER,
+//         process.env.DB_PASSWORD,
+//         {
+//             host: 'localhost',
+//             dialect: 'mysql',
+//             port: 3306
+//         }
+//     );
+// }
 
 module.exports = sequelize;
